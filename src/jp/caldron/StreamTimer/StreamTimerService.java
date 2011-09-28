dc4fc18d50be1dcf8f866be6dd692623b75b2ca1
KURASAWA Nozomu <nabetaro@caldron.jp>
2011-09-29 08:08:42 +0900

    Searvice 追加

---
+++ src/jp/caldron/StreamTimer/StreamTimerService.java
@@ -0,0 +1,15 @@
+package jp.caldron.StreamTimer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class StreamTimerService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
